3ae7de65a175011d62478034e030fc0d8100e1cb
herman-barnardt <hmbarnardt@gmail.com>
2024-12-11 11:29:16 +0200

    chore: Clean up day 10

--- day10/day10.go
+++ day10/day10.go
@@ -14,8 +14,22 @@ func solve2024Day10Part1(lines []string) interface{} {
 	sum := 0
 	for p, v := range grid {
 		if v == 0 {
-			heads, _ := findPaths(grid, p, make(map[util.Point]bool), 0)
-			sum += len(heads)
+			toVist := []util.Point{p}
+			trailHeads := make(map[util.Point]bool)
+			for len(toVist) > 0 {
+				current := toVist[0]
+				toVist = toVist[1:]
+
+				if grid[current] == 9 {
+					trailHeads[current] = true
+				}
+				for _, n := range current.GetAdjacent() {
+					if v, ok := grid[n]; ok && v == grid[current]+1 {
+						toVist = append(toVist, n)
+					}
+				}
+			}
+			sum += len(trailHeads)
 		}
 	}
 	return sum
@@ -26,36 +40,25 @@ func solve2024Day10Part2(lines []string) interface{} {
 	sum := 0
 	for p, v := range grid {
 		if v == 0 {
-			_, numberOfPaths := findPaths(grid, p, make(map[util.Point]bool), 0)
-			sum += numberOfPaths
+			toVist := []util.Point{p}
+			trailHeads := make(map[util.Point]int)
+			for len(toVist) > 0 {
+				current := toVist[0]
+				toVist = toVist[1:]
+
+				if grid[current] == 9 {
+					trailHeads[current] = trailHeads[current] + 1
+				}
+				for _, n := range current.GetAdjacent() {
+					if v, ok := grid[n]; ok && v == grid[current]+1 {
+						toVist = append(toVist, n)
+					}
+				}
+			}
+			for _, v := range trailHeads {
+				sum += v
+			}
 		}
 	}
 	return sum
-}
-
-func getValidNeighbours(grid map[util.Point]int, point util.Point) []util.Point {
-	neighbours := make([]util.Point, 0)
-	for _, n := range point.GetAdjacent() {
-		if v, ok := grid[n]; ok && v == grid[point]+1 {
-			neighbours = append(neighbours, n)
-		}
-	}
-	return neighbours
-}
-
-func findPaths(grid map[util.Point]int, start util.Point, heads map[util.Point]bool, numberOfPaths int) (map[util.Point]bool, int) {
-	if grid[start] == 9 {
-		heads[start] = true
-		return heads, numberOfPaths + 1
-	}
-	neighbours := getValidNeighbours(grid, start)
-	if len(neighbours) == 0 {
-		return heads, numberOfPaths
-	}
-
-	for _, n := range neighbours {
-		heads, numberOfPaths = findPaths(grid, n, heads, numberOfPaths)
-	}
-
-	return heads, numberOfPaths
 }
