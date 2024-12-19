da0f5ad4d5ebadead70da6d48514bfe75a8981f8
herman-barnardt <hmbarnardt@gmail.com>
2024-12-19 08:10:54 +0200

    feat: Solve day 19

--- day19/day19.go
+++ day19/day19.go
@@ -1,8 +1,10 @@
 package day19
+
 import (
-	"errors"
+	"strings"
 
 	aoc "github.com/herman-barnardt/aoc"
+	"github.com/herman-barnardt/aoc/util"
 )
 
 func init() {
@@ -10,9 +12,49 @@ func init() {
 }
 
 func solve2024Day19Part1(lines []string, test bool) interface{} {
-	return errors.New("Not yet implemented")
+	parts := util.SplitLines(lines, "")
+	towels := strings.Split(parts[0][0], ", ")
+	patterns := parts[1]
+
+	count := 0
+
+	for _, pattern := range patterns {
+		if possibleCount(towels, pattern, make(map[string]int)) > 0 {
+			count++
+		}
+	}
+
+	return count
 }
 
 func solve2024Day19Part2(lines []string, test bool) interface{} {
-	return errors.New("Not yet implemented")
+	parts := util.SplitLines(lines, "")
+	towels := strings.Split(parts[0][0], ", ")
+	patterns := parts[1]
+
+	sum := 0
+
+	for _, pattern := range patterns {
+		sum += possibleCount(towels, pattern, make(map[string]int))
+	}
+
+	return sum
+}
+
+func possibleCount(towels []string, pattern string, cache map[string]int) int {
+	if _, ok := cache[pattern]; !ok {
+		if len(pattern) == 0 {
+			cache[pattern] = 1
+			return 1
+		}
+
+		sum := 0
+		for _, towel := range towels {
+			if strings.HasPrefix(pattern, towel) {
+				sum += possibleCount(towels, pattern[len(towel):], cache)
+			}
+		}
+		cache[pattern] = sum
+	}
+	return cache[pattern]
 }
