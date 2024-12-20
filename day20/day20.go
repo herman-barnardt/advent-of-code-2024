0983ba0cf235a8e76c7032f599f411d7ccbd04a1
herman-barnardt <hmbarnardt@gmail.com>
2024-12-20 17:05:11 +0200

    feat: Solve day 20

--- day20/day20.go
+++ day20/day20.go
@@ -1,8 +1,11 @@
 package day20
+
 import (
-	"errors"
+	"slices"
 
 	aoc "github.com/herman-barnardt/aoc"
+	"github.com/herman-barnardt/aoc/graph"
+	"github.com/herman-barnardt/aoc/util"
 )
 
 func init() {
@@ -10,9 +13,113 @@ func init() {
 }
 
 func solve2024Day20Part1(lines []string, test bool) interface{} {
-	return errors.New("Not yet implemented")
+	maze := util.LinesToPointMapOfBasicNodes(lines, func(neighbour *graph.BasicNode) bool {
+		return neighbour.Value != "#"
+	})
+	var start, end *graph.BasicNode
+
+	for _, n := range maze {
+		if n.Value == "S" {
+			start = n
+		}
+		if n.Value == "E" {
+			end = n
+		}
+		if start != nil && end != nil {
+			break
+		}
+	}
+
+	nodePath, floatDistance, _ := graph.FindShortestPath(start, end)
+
+	distance := int(floatDistance)
+
+	path := make([]*graph.BasicNode, 0)
+
+	for _, n := range nodePath {
+		path = append(path, n.(*graph.BasicNode))
+	}
+
+	distanceFromStart := make(map[util.Point]int)
+
+	for i, n := range path {
+		p := util.Point{X: n.X, Y: n.Y}
+		distanceFromStart[p] = distance - i
+	}
+
+	count := 0
+
+	for _, s := range path {
+		startPoint := util.Point{X: s.X, Y: s.Y}
+		for x := -2; x <= 2; x++ {
+			maxY := 2 - util.IntAbs(x)
+			for y := -maxY; y <= maxY; y++ {
+				p := util.Point{X: s.X + x, Y: s.Y + y}
+				if n, ok := maze[p]; ok && slices.Contains(path, n) && !s.Equal(n) {
+					distanceBetween := util.DistanceBetween(&startPoint, &p)
+					if distanceFromStart[p]-distanceBetween-distanceFromStart[startPoint] >= 100 {
+						count++
+					}
+				}
+			}
+		}
+	}
+
+	return count
 }
 
 func solve2024Day20Part2(lines []string, test bool) interface{} {
-	return errors.New("Not yet implemented")
+	maze := util.LinesToPointMapOfBasicNodes(lines, func(neighbour *graph.BasicNode) bool {
+		return neighbour.Value != "#"
+	})
+	var start, end *graph.BasicNode
+
+	for _, n := range maze {
+		if n.Value == "S" {
+			start = n
+		}
+		if n.Value == "E" {
+			end = n
+		}
+		if start != nil && end != nil {
+			break
+		}
+	}
+
+	nodePath, floatDistance, _ := graph.FindShortestPath(start, end)
+
+	distance := int(floatDistance)
+
+	path := make([]*graph.BasicNode, 0)
+
+	for _, n := range nodePath {
+		path = append(path, n.(*graph.BasicNode))
+	}
+
+	distanceFromStart := make(map[util.Point]int)
+
+	for i, n := range path {
+		p := util.Point{X: n.X, Y: n.Y}
+		distanceFromStart[p] = distance - i
+	}
+
+	count := 0
+
+	for _, s := range path {
+		startPoint := util.Point{X: s.X, Y: s.Y}
+		for x := -20; x <= 20; x++ {
+			maxY := 20 - util.IntAbs(x)
+			for y := -maxY; y <= maxY; y++ {
+				p := util.Point{X: s.X + x, Y: s.Y + y}
+				if n, ok := maze[p]; ok && slices.Contains(path, n) && !s.Equal(n) {
+					distanceBetween := util.DistanceBetween(&startPoint, &p)
+					if distanceFromStart[p]-distanceBetween-distanceFromStart[startPoint] >= 100 {
+						count++
+					}
+				}
+			}
+		}
+	}
+
+	return count
 }
